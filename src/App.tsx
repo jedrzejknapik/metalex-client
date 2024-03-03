b7b0767ccbbdb8acfd906acb16e65bc23641e420
jedrzej.knapik@miquido.com <jedrzej.knapik@b-connected.io>
2024-03-03 13:16:39 +0100

    feat(general): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import "./App.css";
+import { Button } from "antd";
+
+const App: FC = () => {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default App;
