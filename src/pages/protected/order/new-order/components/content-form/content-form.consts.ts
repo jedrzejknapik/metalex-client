6d2a9708dfa20f8fc46db21ebe9f6bd97ba7efc0
Konrad Duleba <kon.duleba@gmail.com>
2024-04-14 14:10:28 +0200

    Fix - Incorrect data type

--- src/pages/protected/order/new-order/components/content-form/content-form.consts.ts
+++ src/pages/protected/order/new-order/components/content-form/content-form.consts.ts
@@ -2,10 +2,11 @@ import { CreateOrderFormFields, CreateOrderPayload } from '@types';
 
 export const INITIAL_CONTENT_FORM_VALUES: Omit<
   CreateOrderPayload,
-  CreateOrderFormFields.PRICE | CreateOrderFormFields.PRODUCTS_QUANTITY
+  | CreateOrderFormFields.PRICE
+  | CreateOrderFormFields.PRODUCTS_QUANTITY
+  | CreateOrderFormFields.CREATED_AT
 > = {
   customerId: '',
-  createdAt: '',
   orderNr: '',
   positions: [],
 };
